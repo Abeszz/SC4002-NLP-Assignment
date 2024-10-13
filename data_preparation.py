136318bed08591ee80f8d6fdae77e2c64bdcce65
Abe <abraham_1360@hotmail.com>
2024-10-13 20:57:17 +0800

    update setup for nltk tokenizer

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,11 @@
 ﻿from datasets import load_dataset
 
+train_dataset = None
+validation_dataset = None
+test_dataset = None
+
 def load_datasets():
+    global train_dataset, validation_dataset, test_dataset
     dataset = load_dataset('rotten_tomatoes')
     train_dataset = dataset['train']
     validation_dataset = dataset['validation']
