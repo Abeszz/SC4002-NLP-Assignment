cd2e3fbd062fc1addf37ce78806ffa579d4504de
Abe <abraham_1360@hotmail.com>
2024-10-15 01:57:42 +0800

    change project to use jupyter notebook

--- constants.py
+++
@@ -1,7 +0,0 @@
-﻿# This file contains all constants variable
-
-# GloVe model configuration
-glove_model_path = './GlovePreTrainedModels/glove.6B/glove.6B.300d.txt'
-glove_model_dimension = 300   # Specify the number of dimensions of the chosen model
-
-embedding_matrix_path = './partone/glove_embedding_matrix.npy'
